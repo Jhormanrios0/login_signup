3a8f6f76316dd8e85eb79f9fadb98f517c80897b
jhorman <78864961+Jhormanrios0@users.noreply.github.com>
2025-03-11 15:54:10 -0500

    initialize Vue 3 project with Vite, Tailwind CSS, and basic structure

---
+++ login_signup/src/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LoginAuth from "./components/Auth/LoginAuth.vue";
+</script>
+
+<template>
+  <main class="min-h-[85vh] flex flex-col items-center justify-center">
+    <LoginAuth />
+  </main>
+</template>
---
+++ login_signup/src/components/Auth/LoginAuth.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="text-black">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis eligendi
+    expedita natus iure nobis eos, qui maxime quo aspernatur dolor fugit. Ab
+    quia quaerat impedit minus officia voluptatum nemo ipsam sint commodi?
+    Eaque, quod? In quas optio nihil ut culpa?
+  </h1>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #333;
+}
+</style>
